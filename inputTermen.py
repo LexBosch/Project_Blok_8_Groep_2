61b8e5e8b737686e035a29028065cd2e0b1e9ecb
LexBosch <lexbosch@live.nl>
2019-05-29 10:17:37 +0200

    fancy input to graph

--- inputTermen.py
+++ inputTermen.py
@@ -4,20 +4,20 @@ from datetime import date
 
 
 class pubMedThread(threading.Thread):
-    def __init__(self, termList, sessionName, currentDate, threadName):
+    def __init__(self, termList, sessionName, currentDate, email):
         threading.Thread.__init__(self)
-        self.threadName = threadName
         self.termList = termList
         self.sessionName = sessionName
         self.currentDate = currentDate
+        self.email = email
 
     def run(self):
         # call textmining
         print()
 
 
-def StartPubMedSearch(searchTermList, sessionName, threadName):
+def StartPubMedSearch(searchTermList, sessionName, email):
     today = date.today()
     currentDate = today.strftime("%d/%m/%Y")
-    newThread = pubMedThread(searchTermList, sessionName, currentDate, threadName)
+    newThread = pubMedThread(searchTermList, sessionName, currentDate, email)
     newThread.start()
